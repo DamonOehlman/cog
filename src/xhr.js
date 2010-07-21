c985bcc3a52e2e4d3749688e3f48e95f1d1df3b5
Damon Oehlman <damon.oehlman@gmail.com>
2010-07-21 12:08:37 +1000

    Fixed JSON variable scoping

--- src/xhr.js
+++ src/xhr.js
@@ -5,12 +5,13 @@ The JSONP namespace provides a lightweight JSONP implementation.  This code
 is implemented as-is from the code released on www.nonobtrusive.com, as per the
 blog post listed below.  Only two changes were made. First, rename the json function
 to get around jslint warnings. Second, remove the params functionality from that
-function (not needed for my implementation).
+function (not needed for my implementation).  Oh, and fixed some scoping with the jsonp
+variable (didn't work with multiple calls).
 
 http://www.nonobtrusive.com/2010/05/20/lightweight-jsonp-without-any-3rd-party-libraries/
 */
 GRUNT.JSONP = (function(){
-    var counter = 0, head, query, key, jsonp, window = this;
+    var counter = 0, head, query, key, window = this;
     
     function load(url) {
         var script = document.createElement('script'),
@@ -37,7 +38,7 @@ GRUNT.JSONP = (function(){
         // apply either a ? or & to the url depending on whether we already have query params
         url += url.indexOf("?") >= 0 ? "&" : "?";
 
-        jsonp = "json" + (++counter);
+        var jsonp = "json" + (++counter);
         window[ jsonp ] = function(data){
             callback(data);
             window[ jsonp ] = null;
