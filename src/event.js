e29da2fa45da7f6e30e6b3c551eb82c1bcb813e3
Damon Oehlman <damon.oehlman@gmail.com>
2010-08-25 12:56:20 +1000

    Decided the singleton instance was silly

--- src/event.js
+++ src/event.js
@@ -1,21 +1,18 @@
-GRUNT.Observable = (function() {
-    var callbackCounter = 0;
+GRUNT.Observable = function() {
+    var listeners = {},
+        callbackCounter = 0;
     
-    var module = {
-        bind: function(target, eventName, callback) {
-            if (! target.observableEvents) {
-                target.observableEvents = {};
-            } // if
-    
-            if (! target.observableEvents[eventName]) {
-                target.observableEvents[eventName] = [];
+    var self = {
+        bind: function(eventName, callback) {
+            if (! listeners[eventName]) {
+                listeners[eventName] = [];
             } // if
             
             // increment the event counter
             callbackCounter += 1;
             
             // add the callback to the list of listeners
-            target.observableEvents[eventName].push({
+            listeners[eventName].push({
                 callback: callback,
                 callbackId: callbackCounter
             });
@@ -23,36 +20,32 @@ GRUNT.Observable = (function() {
             return callbackCounter;
         },
         
-        trigger: function(target, eventName) {
-            if (target.observableEvents) {
-                var eventCallbacks = target.observableEvents[eventName];
-
-                // check that we have callbacks
-                if (! eventCallbacks) {
-                    return;
-                } // if
-
-                for (var ii = eventCallbacks.length; ii--; ) {
-                    eventCallbacks[ii].callback.apply(self, Array.prototype.slice.call(arguments, 2));
-                } // for
+        trigger: function(eventName) {
+            var eventCallbacks = listeners[eventName];
+                
+            // check that we have callbacks
+            if (! eventCallbacks) {
+                return;
             } // if
+            
+            for (var ii = eventCallbacks.length; ii--; ) {
+                eventCallbacks[ii].callback.apply(self, Array.prototype.slice.call(arguments, 1));
+            } // for
         },
         
-        unbind: function(target, eventName, callbackId) {
-            if (target.observableEvents) {
-                var eventCallbacks = target.observableEvents[eventName];
-                for (var ii = 0; eventCallbacks && (ii < eventCallbacks.length); ii++) {
-                    if (eventCallbacks[ii].callbackId === callbackId) {
-                        eventCallbacks.splice(ii, 1);
-                        break;
-                    } // if
-                } // for
-            } // if
+        unbind: function(eventName, callbackId) {
+            var eventCallbacks = listeners[eventName];
+            for (var ii = 0; eventCallbacks && (ii < eventCallbacks.length); ii++) {
+                if (eventCallbacks[ii].callbackId === callbackId) {
+                    eventCallbacks.splice(ii, 1);
+                    break;
+                } // if
+            } // for
         }
     };
     
-    return module;
-})(); 
+    return self;
+}; 
 
 // TODO: add functionality that allows you to stop listening to messages
 GRUNT.WaterCooler = (function() {
