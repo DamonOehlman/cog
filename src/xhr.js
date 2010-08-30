cbcbe3169e604946fb846f5070a0a49b82715acf
Damon Oehlman <damon.oehlman@gmail.com>
2010-08-30 13:31:31 +1000

    Allowed customization of the callback parameters

--- src/xhr.js
+++ src/xhr.js
@@ -34,7 +34,7 @@ GRUNT.JSONP = (function(){
         head.appendChild( script );
     } // load
     
-    function prepAndLoad(url, callback) {
+    function prepAndLoad(url, callback, callbackParam) {
         // apply either a ? or & to the url depending on whether we already have query params
         url += url.indexOf("?") >= 0 ? "&" : "?";
 
@@ -47,7 +47,7 @@ GRUNT.JSONP = (function(){
             } catch (e) {}
         };
  
-        load(url + "callback=" + jsonp);
+        load(url + (callbackParam ? callbackParam : "callback") + "=" + jsonp);
         return jsonp;
     } // jsonp
     
