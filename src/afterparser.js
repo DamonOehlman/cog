f02fbef44f22d771c8cf9d50c68f87f51d573e73
Damon Oehlman <damon.oehlman@gmail.com>
2010-09-08 21:04:38 +1000

    Added async string parser

---
+++ src/afterparser.js
@@ -0,0 +1,47 @@
+GRUNT.ParseRules = function(params) {
+    var rules = [];
+    
+    var self = {
+        add: function(regex, handler) {
+            rules.push({
+                regex: regex,
+                handler: handler
+            });
+        },
+        
+        each: function(input, outputReceiver, allCompleteCallback) {
+            var completionCounter = 0;
+            
+            function incCounter() {
+                completionCounter++;
+                if (allCompleteCallback && (completionCounter >= rules.length)) {
+                    allCompleteCallback(outputReceiver);
+                } // if
+            } // incCounter
+            
+            for (var ii = 0; ii < rules.length; ii++) {
+                var regex = rules[ii].regex,
+                    handler = rules[ii].handler,
+                    handled = false;
+                
+                if (regex) {
+                    regex.lastIndex = -1;
+                    
+                    var matches = regex.exec(input);
+                    if (matches && handler) {
+                        handled = true;
+                        if (handler(matches, outputReceiver, incCounter)) {
+                            incCounter();
+                        } // if
+                    } // if
+                } // if
+                
+                if (! handled) {
+                    incCounter();
+                } // if
+            }
+        }
+    };
+    
+    return self;
+}; // ParseRules
