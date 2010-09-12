d5e0549f5a2bef2f4c7db5c129892245deb7bf35
Damon Oehlman <damon.oehlman@gmail.com>
2010-09-12 13:54:00 +1000

    Removed WaterCooler for terseness - now just GT.say and GT.listen

--- src/watercooler.js
+++ src/watercooler.js
@@ -1,53 +1,45 @@
 // TODO: add functionality that allows you to stop listening to messages
-GT.WaterCooler = (function() {
+(function() {
     // initialise variables
     var messageListeners = {},
         pipes = [];
     
     // define the module
-    var module = {
-        addPipe: function(callback) {
-            // test the pipe because if it is broke it will break everything
-            callback("pipe.test", {});
-            
-            // given that didn't throw an exception and we got here, we can now add the pipe
-            pipes.push(callback);
-        },
+    GT.addPipe = function(callback) {
+        // test the pipe because if it is broke it will break everything
+        callback("pipe.test", {});
         
-        listen: function(message, callback) {
-            // if we don't have a message listener array configured, then create one now
-            if (! messageListeners[message]) {
-                messageListeners[message] = [];
-            } // if
-            
-            // add the callback to the listener queue
-            if (callback) {
-                messageListeners[message].push(callback);
-            } // if
-        },
-        
-        say: function(message, args) {
-            var ii;
-            
-            // if there are pipes, then send the message through each
-            for (ii = pipes.length; ii--; ) {
-                pipes[ii](message, args);
-            } // for
-            
-            // if we don't have any message listeners for that message, then return
-            if (! messageListeners[message]) { return; }
-            
-            // iterate through the message callbacks
-            for (ii = messageListeners[message].length; ii--; ) {
-                messageListeners[message][ii](args);
-            } // for
-        },
-        
-        leave: function() {
-            
-        }
-    };
+        // given that didn't throw an exception and we got here, we can now add the pipe
+        pipes.push(callback);
+    }; // addPipe
     
-    return module;
+    GT.listen = function(message, callback) {
+        // if we don't have a message listener array configured, then create one now
+        if (! messageListeners[message]) {
+            messageListeners[message] = [];
+        } // if
+        
+        // add the callback to the listener queue
+        if (callback) {
+            messageListeners[message].push(callback);
+        } // if
+    }; // listen
+        
+    GT.say = function(message, args) {
+        var ii;
+        
+        // if there are pipes, then send the message through each
+        for (ii = pipes.length; ii--; ) {
+            pipes[ii](message, args);
+        } // for
+        
+        // if we don't have any message listeners for that message, then return
+        if (! messageListeners[message]) { return; }
+        
+        // iterate through the message callbacks
+        for (ii = messageListeners[message].length; ii--; ) {
+            messageListeners[message][ii](args);
+        } // for
+    }; // say
 })();
 
