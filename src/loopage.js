44cd23b6ae2bef8feec914d0166931f8de80d683
Damon Oehlman <damon.oehlman@gmail.com>
2010-10-01 11:36:21 +1000

    Added GT.Loopage module

---
+++ src/loopage.js
@@ -0,0 +1,57 @@
+/**
+GT.Loopage
+----------
+
+This module implements a control loop that can be used to centralize
+jobs draw loops, animation calculations, partial calculations for GT.Job 
+instances, etc.
+*/
+GT.Loopage = (function() {
+    // initialise variables
+    var workerCount = 0,
+        workers = [],
+        loopTimeout = 0;
+    
+    function LoopWorker(params) {
+        var self = GT.extend({
+            id: workerCount++,
+            execute: function() {}
+        }, params);
+        
+        return self;
+    } // LoopWorker
+    
+    
+    /* internal functions */
+    
+    function joinLoop(params) {
+        // create the worker
+        var worker = new LoopWorker(params);
+        
+        // make the worker observable
+        GT.observable(worker);
+        worker.bind('complete', leaveLoop);
+        
+        // if the loop is not running, then set it running
+        loopTimeout = loopTimeout ? loopTimeout : setTimeout(runLoop, 0);
+    } // joinLoop
+    
+    function leaveLoop(workerId) {
+        
+    } // leaveLoop
+    
+    function runLoop() {
+        // get the current tick count
+        var tickCount = new Date().getTime();
+        
+        // iterate through the workers and run
+        for (var ii = workers.length; ii--; ) {
+            workers[ii].execute(tickCount, workers[ii]);
+        } // for
+    } // runLoop
+    
+    var module = {
+        join: joinLoop,
+        leave: leaveLoop
+    };
+})();
