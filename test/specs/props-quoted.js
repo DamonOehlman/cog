5cee49b97231b70adf2aa0d794065281e0ab4e9f
Damon Oehlman <damon.oehlman@sidelab.com>
2012-06-08 13:52:45 +1000

    Restructuring the library

--- test/specs/props-quoted.js
+++
@@ -1,6 +0,0 @@
-describe('multiprop parser - quoted items', function() {
-    it('can parse a single item, with a quoted value', function() {
-        var props = _parseprops('font-family:"Courier New"');
-        expect(props['font-family']).toEqual('Courier New');
-    });
-});
--- test/specs/typetools.js
+++
@@ -1,7 +0,0 @@
-describe('typetools', function() {
-    it('should be able to detect an array', function() {
-        var testArray = [];
-        
-        expect(_is(testArray, 'array')).toBeTruthy();
-    });
-});
