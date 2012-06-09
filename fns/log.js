cb381321283b4b5f05c81f436fa92eb4ebc822b0
Damon Oehlman <damon.oehlman@sidelab.com>
2012-06-09 15:16:59 +1000

    Fixed logger

--- fns/log.js
+++ fns/log.js
@@ -1,12 +1,5 @@
-function _log(msg, level) {
+cog.log = function(msg, level) {
     if (typeof console !== 'undefined') {
         console[level || 'log'](msg);
     } // if
-} // _log
-
-function _logError(error) {
-    if (typeof console !== 'undefined') {
-        console.error(error);
-        console.log(error.stack);
-    } // if
-} // _logError
+};
