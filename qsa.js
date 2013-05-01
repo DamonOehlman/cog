e40e3362dd29e9dd82e054fab7db06f5832f7de0
Damon Oehlman <damon.oehlman@sidelab.com>
2013-05-01 11:15:20 +1000

    Adding qsa from snippets so snippets can be deleted

---
+++ qsa.js
@@ -0,0 +1,32 @@
+/*
+ * ## qsa(selector, element)
+ * 
+ * This function is used to get the results of the querySelectorAll output in the fastest
+ * possible way.  This code is very much based on the implementation in 
+ * [zepto](https://github.com/madrobby/zepto/blob/master/src/zepto.js#L104), but perhaps 
+ * not quite as terse.
+ */
+var classSelectorRE = /^\.([\w\-]+)$/,
+    idSelectorRE = /^#([\w\-]+)$/,
+    tagSelectorRE = /^[\w\-]+$/;
+
+module.exports = function(selector, scope) {
+    var idSearch;
+    
+    // default the element to the document
+    scope = scope || document;
+    
+    // determine whether we are doing an id search or not
+    idSearch = scope === document && idSelectorRE.test(selector);
+    
+    // perform the search
+    return idSearch ? 
+        // we are doing an id search, return the element search in an array
+        [scope.getElementById(RegExp.$1)] :
+        // not an id search, call the appropriate selector
+        Array.prototype.slice.call(
+            classSelectorRE.test(selector) ? scope.getElementsByClassName(RegExp.$1) :
+            tagSelectorRE.test(selector) ? scope.getElementsByTagName(selector) : 
+            scope.querySelectorAll(selector)
+        );
+};
