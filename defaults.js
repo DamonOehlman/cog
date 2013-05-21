dc6c2a0e7c7a944473e53024bed526d8eea0bb4b
Damon Oehlman <damon.oehlman@sidelab.com>
2013-05-21 16:28:28 +1000

    Ensure defaults can be handled if undefined is passed in as the target

--- defaults.js
+++ defaults.js
@@ -3,6 +3,10 @@
 
 */
 module.exports = function(target) {
+    // ensure we have a target
+    target = target || {};
+
+    // iterate through the sources and copy to the target
     [].slice.call(arguments, 1).forEach(function(source) {
         if (! source) return;
 
--- test/defaults.js
+++ test/defaults.js
@@ -3,7 +3,8 @@ var test = require('tape'),
 
 test('defaults', function(t) {
 
-    t.plan(2);
+    t.plan(3);
     t.equal(defaults({}, { a: true }).a, true, 'Property a copied across');
     t.equal(defaults({ a: 1 }, { a: 2 }).a, 1, 'Property value not overriden');
+    t.equal(defaults(undefined, { a: true }).a, true, 'Property pushed to new object');
 });
