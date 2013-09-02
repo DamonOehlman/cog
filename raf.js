bbbcd36b8ae1c337815c4185db0961c88c299616
Damon Oehlman <damon.oehlman@gmail.com>
2013-09-03 09:56:12 +1000

    Added raf functionality

---
+++ raf.js
@@ -0,0 +1,14 @@
+/* jshint node: true */
+/* global window: false */
+'use strict';
+
+var TEST_PROPS = ['r', 'webkitR', 'mozR', 'oR', 'msR'];
+
+module.exports = typeof window != 'undefined' && (function() {
+  for (var ii = 0; ii < TEST_PROPS.length; ii++) {
+    window.animFrame = window.animFrame ||
+      window[TEST_PROPS[ii] + 'equestAnimationFrame'];
+  } // for
+  
+  return animFrame;
+})();
