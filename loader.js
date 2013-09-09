dfb3dfe801d287f71435a0f3d1861652c45278c3
Damon Oehlman <damon.oehlman@gmail.com>
2013-09-09 12:35:51 +1000

    Added simple script loader to the toolbag

---
+++ loader.js
@@ -0,0 +1,59 @@
+/* jshint node: true */
+/* global document: false */
+'use strict';
+
+/**
+  ## loader(urls, opts, callback)
+
+  ```js
+  var loader = require('cog/loader');
+  ```
+
+  This is a simple script loader that will load the urls specified
+  and trigger the callback once all those scripts have been loaded (or
+  loading has failed in one instance).
+
+**/
+
+module.exports = function(urls, opts, callback) {
+  var pending;
+  var target;
+
+  function handleLoaded(evt) {
+    // decrement the pending count
+    pending -= 1;
+
+    // if we have finished, trigger the callback
+    if (pending <= 0 && typeof callback == 'function') {
+      // TODO: detect and send through errors
+      callback();
+    } 
+  }
+
+  // ensure we have an array for scripts
+  urls = [].concat(urls || []);
+
+  // check the no opts case
+  if (typeof opts == 'function') {
+    callback = opts;
+    opts = {};
+  }
+
+  // set how many urls we are waiting for
+  pending = urls.length;
+
+  // initialise the target (default to document.body)
+  target = (opts || {}).target || document.body;
+
+  // create the script elements
+  urls.map(function(url) {
+    var script = document.createElement('script');
+    script.url = url;
+    script.async = true;
+    script.addEventListener('load', handleLoaded);
+
+    return script;
+  })
+  // add them to the target
+  .forEach(target.appendChild.bind(target));
+};
