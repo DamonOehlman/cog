4c739c7702b9cd01b87878207f3e4fc774c7c880
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-27 22:23:38 +1000

    Added getable implementation

---
+++ getable.js
@@ -0,0 +1,36 @@
+module.exports = function(target) {
+  function get(key) {
+    return target[key];
+  }
+
+  function set(key, value) {
+    target[key] = value;
+  }
+
+  function remove(key) {
+    return delete target[key];
+  }
+
+  function keys() {
+    return Object.keys(target);
+  };
+
+  function values() {
+    return Object.keys(target).map(function(key) {
+      return target[key];
+    });
+  };
+
+  if (typeof target != 'object') {
+    return target;
+  }
+
+  return {
+    get: get,
+    set: set,
+    remove: remove,
+    delete: remove,
+    keys: keys,
+    values: values
+  };
+};
