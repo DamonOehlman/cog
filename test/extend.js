c7a84cc04098dccb83245e7e229dbc4c66ffb8c1
Damon Oehlman <damon.oehlman@gmail.com>
2014-07-15 17:37:17 +1000

    Added test to ensure empty attribute values are extended correctly

--- test/extend.js
+++ test/extend.js
@@ -5,3 +5,8 @@ test('simple extends', function(t) {
   t.plan(1);
   t.equal(extend({}, { a: true }).a, true, 'Property a copied across');
 });
+
+test('can override an attribute with an empty string update', function(t) {
+  t.plan(1);
+  t.equal(extend({}, { a: 'test' }, { a: '' }).a, '', 'Property a copied across');
+});
